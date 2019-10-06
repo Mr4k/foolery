a94f8218c474a8a1440ead19a883849c1ba69354
Peter Stefek <peterlikesemail@gmail.com>
2019-10-06 09:50:50 -0400

    fixed friction / normal forces + added better controls

--- js/physics.js
+++ js/physics.js
@@ -6,15 +6,15 @@ function calculateSlide(velocity, t, tMax, normal) {
 	return geom.add(scaledV, geom.scale(normal, -geom.dot(scaledV, normal)));
 }
 
-function moveAndSlide(origin, radius, velocity, triangles, stepSize = 1, depth = 3, kineticFriction = 0.5) {
-	if (depth === 0) return origin;
+function moveAndSlide(origin, radius, velocity, triangles, stepSize = 1, depth = 3, kineticFriction = 0.5, totalNormalForce = { x: 0, y: 0, z: 0 }, totalFrictionForce = { x: 0, y: 0, z: 0 }) {
+	if (depth === 0) return { newOrigin: origin, totalNormalForce, totalFrictionForce };
 
 	let minT = 10000000000;
 	let minN;
 
 	for (let triangle of triangles) {
 		const intersection = geom.sphereHitsTriangle(origin, velocity,
-		 radius, triangle);
+			radius, triangle);
 		if (intersection && intersection.t >= 0 && intersection.t < minT) {
 			minN = intersection.n;
 			minT = intersection.t;
@@ -25,21 +25,29 @@ function moveAndSlide(origin, radius, velocity, triangles, stepSize = 1, depth =
 
 	const move = geom.add(origin, geom.scale(velocity, minT));
 
-	if (!minN) return move;
+	if (!minN) return { newOrigin: move, totalNormalForce, totalFrictionForce };
 
 	const slide = calculateSlide(velocity, minT, stepSize, minN);
 
 	// TODO I'm sure this part could be improved / moved
 	// also test this
 	const normalForceMag = Math.abs(geom.dot(minN, velocity)) * (1 - minT);
-	const postFrictionSlide = geom.scale(slide, Math.max(Math.sqrt(geom.dot(slide, slide)) - normalForceMag * kineticFriction, 0));
+	const updatedTotalNormalForce = geom.add(totalNormalForce, geom.scale(minN, normalForceMag));
+	
+	const slideMag = Math.sqrt(geom.dot(slide, slide));
+	const frictionAmount = Math.max(slideMag - normalForceMag * kineticFriction, 0) / (slideMag === 0 ? 1 : slideMag);
+	const postFrictionSlide = geom.scale(slide, frictionAmount);
+	const updatedTotalFrictionForce = geom.add(
+		totalFrictionForce,
+		geom.scale(slide, -(1 - frictionAmount))
+	);
 
-	return moveAndSlide(move, radius, postFrictionSlide, triangles, stepSize, depth - 1);
+	return moveAndSlide(move, radius, postFrictionSlide, triangles, stepSize, depth - 1, kineticFriction, updatedTotalNormalForce, updatedTotalFrictionForce);
 }
 
 // TODO this function could be wayyyy more efficient
 function calculateGravityDirection(origin, triangles, exclusionDist = 100) {
-	const { grav, _minDist } = triangles.reduce(( { grav, minDist }, tri) => {
+	const { grav, _minDist } = triangles.reduce(({ grav, minDist }, tri) => {
 		let ret = geom.squaredDistToTriangle(origin, tri);
 		if (!ret) return { grav, minDist };
 
@@ -69,10 +77,6 @@ function calculateGravityDirection(origin, triangles, exclusionDist = 100) {
 	if (grav.x === 0 && grav.y === 0 && grav.z === 0) return grav;
 
 	return geom.scale(geom.normalize(grav), -1);
-}
-
-function getForward(gravDir, oldGravDir, oldFwd) {
-	
 }
 
 module.exports = {
